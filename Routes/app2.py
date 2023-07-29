27d470134c8b737ee99eb29278093f9b554ede1a
Irish <ajaybhartiirish@gmail.com>
2023-07-29 23:24:47 +0530

    none

--- Routes/app2.py
+++ Routes/app2.py
@@ -1,25 +1,31 @@
+import fastapi
+import email_validator
+from fastapi import FastAPI, Depends, HTTPException, Request, Form, status
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from pydantic import EmailStr, BaseModel
+from middleware.token_middleware import TestMiddleware
+from models import userinfo
+from sqlalchemy.orm import Session
+from controller.controller import generate_token, get_hash, authenticate_user, get_db
+
 import pickle
 import re
 import nltk
-import fastapi
-from fastapi import FastAPI
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 import string
-import os
 
 nltk.download('stopwords')
 
-file_path = os.path.abspath("Routes/tfidf_vectorizer.pkl")
-file_path2 = os.path.abspath("Routes/automl_model.pkl")
-
-with open(file_path, 'rb') as file:
+with open('tfidf_vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
 
-with open(file_path2, 'rb') as file:
+with open('automl_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 stopwords = nltk.corpus.stopwords.words('english')
+
 
 def clean_text(text):
     text = "".join([word.lower() for word in text if word not in string.punctuation])
@@ -27,7 +33,88 @@ def clean_text(text):
     text = [word for word in tokens if word not in stopwords]
     return " ".join(text)
 
-app2 = FastAPI()
+
+app = FastAPI()
+
+# Middleware to be used
+app.add_middleware(TestMiddleware)
+
+templates = Jinja2Templates(directory="view")
 
 
+@app.get("/register")
+def registration_page(request: Request):
+    return templates.TemplateResponse("registration.html", {"request": request})
 
+
+@app.post("/register")
+def user_register(name: str = Form('name'), email: EmailStr = Form('email'), password: str = Form('password'),
+                  db: Session = Depends(get_db)):
+    # email validation
+    try:
+        valid = email_validator.validate_email(email=email)
+
+    except email_validator.EmailNotValidError:
+        raise fastapi.HTTPException(status_code=404, detail="Invalid email")
+
+    # Password Hashing
+    hashed_password = get_hash(password)
+    user = userinfo.User(name=name, email=valid.email, password=hashed_password)
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    response = RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
+    return response
+
+
+@app.get("/login")
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.post("/login")
+def user_login(email: str = Form('email'), password: str = Form("password")):
+    if authenticate_user(email, password):
+        token = generate_token(email)
+        response = RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+        response.set_cookie(key="token", value=token, httponly=True)
+        return response
+    else:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+
+
+# Using static method
+# @app.get("/dashboard")
+# def dashboard(request: Request, response: Response):
+#     username = AuthenticationMiddleware.authMiddle(request, response)
+#     if isinstance(username, Response): return username
+#     print(username)
+#     return f"Welcome {username}"
+
+
+@app.get("/dashboard")
+def user_dashboard(request: Request):
+    username = request.state.username
+    print(username)
+    return f"Welcome {username}"
+
+
+@app.get("/predict")
+def predict(request: Request):
+    return templates.TemplateResponse("dashBoard.html", {"request": request})
+
+
+@app.post("/predict")
+def predict_model(news: str = Form('news')):
+    cleaned_text = clean_text(news)
+    x = vectorizer.transform([cleaned_text])
+
+    prediction = model.predict(x.toarray())
+
+    prediction2 = int(prediction[0])
+
+    if prediction2 == 0:
+        return {"prediction": "Real"}
+    else:
+        return {"prediction": "False"}
